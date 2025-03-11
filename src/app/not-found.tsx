2e1b26cbf82209f3549a0f49257fbb7ede7444a0
Johnny Gérard <john@jgerard.dev>
2025-03-11 11:07:20 +0100

    Initial commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import { AppLink } from "@/component/app-link";
+import { Metadata } from "next";
+import { memo } from "react";
+
+export const metadata: Metadata = {
+  title: "404 Not Found",
+};
+
+const NotFound = () => {
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <div className="text-center">
+        <h1>404 Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <AppLink href="/">Back to Home</AppLink>
+      </div>
+    </div>
+  );
+};
+
+export default memo(NotFound);
